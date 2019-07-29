189a89d03ffabe756083e7d633a047b822637de6
davtjon <d.t.jones@ucl.ac.uk>
2019-07-29 22:40:33 +0100

    Added header remarks and added UCL Copyright

--- nn/dmp-softmax/pytorch_dmp_iterdistpred.py
+++ nn/dmp-softmax/pytorch_dmp_iterdistpred.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+# Predict distance probabilities with previous seed model
+
+# By David T. Jones, Jan 2019 */
+
+# Copyright (C) 2019 University College London
+
+# License: GPLv3
+
 from __future__ import print_function
 
 import sys
